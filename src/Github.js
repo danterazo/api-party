3df1fc3b46b62980a2620bb0a532ef4fcfcbff20
Dante Razo <dante@razonet.org>
2018-07-10 11:53:10 -0400

    Add form to Github component

--- src/Github.js
+++ src/Github.js
@@ -1,10 +1,32 @@
 import React, {Component} from 'react'
+import './Github.css'
 
 class Github extends Component {
+	state = {
+		username: '',
+	}
+
+	handleChange = ev =>{
+		this.setState({username: ev.target.value})
+	}
+
 	render(){
 		return (
 			<div className="Github">
-				GitHub (now a component)
+				<img src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png"
+					 alt="GitHub" className="logo"/>
+				<form>
+					<div>
+						<input required autoFocus type="text"
+							   value={this.state.username}
+							   onChange={this.handleChange}/>
+					</div>
+					<div>
+						<button type="submit">
+							Look up Github User
+						</button>
+					</div>
+				</form>
 			</div>
 		)
 	}
